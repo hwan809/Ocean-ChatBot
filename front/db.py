5e5ffee125c0a9fcc5fed5d39348412aa7a1d8b0
hwan809 <55339366+hwan809@users.noreply.github.com>
2024-08-02 01:17:20 +0900

    remove pdfs add db.py

--- front/db.py
+++ front/db.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import json
 from googleapiclient import discovery
 from google.oauth2 import service_account
 
@@ -6,12 +7,17 @@ class GooglesheetUtils:
     spreadsheet_id = st.secrets['SHEET_ID']
     def __init__(self) -> None:
 
+        service_account_info = json.load(st.secrets["spreadsheet"]['service_account'] )
+        self.credentials = service_account.Credentials.from_service_account_info(
+            service_account_info,
+            scopes = ['https://www.googleapis.com/auth/spreadsheets']
+        )
+
         self.credentials = service_account.Credentials.from_service_account_file(
             'front/service_account.json',
             scopes = ['https://www.googleapis.com/auth/spreadsheets']
         )
-        self.service = discovery.build('sheets', 'v4',
-                                       credentials=self.credentials)
+        self.service = discovery.build('sheets', 'v4', credentials=self.credentials)
     def append_data(self, values, range_name) -> None:
         print(self.spreadsheet_id)
         request = self.service.spreadsheets().values().append(
