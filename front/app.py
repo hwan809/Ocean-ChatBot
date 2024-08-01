d02f8f54db68966e579a4f57e54a0f17d9d8de98
hwan809 <55339366+hwan809@users.noreply.github.com>
2024-08-01 16:10:43 +0900

    bug fix sqlite3

--- front/app.py
+++ front/app.py
@@ -16,10 +16,6 @@ from langchain.chains.query_constructor.base import (
 from langchain.chains import RetrievalQA
 openai_api_key = st.secrets['OPENAI_API_KEY']
 
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-
 # Set OpenAI API key
 llm = ChatOpenAI(model_name="gpt-4o", temperature=0.5, openai_api_key=openai_api_key)
 
@@ -29,8 +25,10 @@ def setup_rag_pipeline(_retriever):
     prompt = PromptTemplate.from_template(
     """당신은 부산과학고등학교의 행사 "Ocean ICT"의 도우미 챗봇인 "한바다" 입니다.
     검색된 정보를 사용하여 질문에 답합니다.
+    
+    팀에 대한 정보를 언급할 때 반드시 팀 코드를 같이 언급하세요.
     팀 코드는 대문자와 숫자 두 자리의 조합입니다.
-    팀에 대한 정보를 언급할 때 반드시 팀 코드를 같이 말하세요.
+    
     답을 모른다면 그냥 너의 정보와 함께 너는 Ocean ICT에 대해서만 답변할 수 있다고 말하면 됩니다.
     답을 안다면 있는 정보를 사용해 최대한 자세하게 답변할 수 있도록 합니다. 여러 줄에 걸쳐서 답변하세요.
     한국어로 친절하고, 친근하게 답하세요.
@@ -91,7 +89,7 @@ metadata_field_info = [
 
     AttributeInfo(
         name="Teammate #2 number",
-        description="A team member's student number. The student number is four digits.",
+        description="A team member's student number. The student number is four digits",
         type="string"
     )
 ]
@@ -117,6 +115,20 @@ examples = [
             "query": "환경에 관한 주제로 연구한 팀을 알려줄래?",
             "filter": "NO_FILTER",
         }   
+    ),
+    (
+        "팀 번호가 B로 시작하는 프로젝트의 주제는 어떤 것이 있어?",
+        {
+            "query": "팀 번호가 B로 시작하는 프로젝트의 주제는 어떤 것이 있어?",
+            "filter": "NO_FILTER",
+        }
+    ),
+    (
+        "머신러닝을 사용하지 않은 팀이 있을까?",
+        {
+            "query": "머신러닝을 사용하지 않은 팀이 있을까?",
+            "filter": "NO_FILTER",
+        }
     )
 ]
 
