25c08318f56cfd0717ae197152313e289efbf385
Thomas <0520wns@naver.com>
2024-08-03 10:46:10 +0900

    °Á Çѹø µ¹·ÁºÃÀ½

--- front/app.py
+++ front/app.py
@@ -6,6 +6,8 @@ __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
+#import sqlite3
+
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 
