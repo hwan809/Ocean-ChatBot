7414efd13364e0172d8789aec5035ed6e9c9ac43
Vivace <hwan809@naver.com>
2024-08-08 22:29:20 +0900

    add sqlite3 dependency

--- front/app.py
+++ front/app.py
@@ -2,9 +2,9 @@ import streamlit as st
 from db import GooglesheetUtils
 import datetime
 
-# __import__('pysqlite3')
-# import sys
-# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 import sqlite3
 
