6ac1975acb3c90698856d206dc53b2d558725be8
hwan809 <hwan809@naver.com>
2024-08-09 11:31:39 +0900

    asdf

--- front/app.py
+++ front/app.py
@@ -243,6 +243,8 @@ if prompt := st.chat_input("질문을 입력하세요"):
     
     youtube_link = docs[0].metadata['Youtube link']
     team_code = docs[0].metadata['Team code']
+
+    print(docs[0])
     st.session_state.messages.append({"role": "assistant", "content": response})
 
     play_video = lambda: st.session_state.messages.append({"role": "video", "content": youtube_link})
