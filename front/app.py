79b4cfcf41ffe8dc810ffb99fc2cb22bf1c6a1f2
hwan809 <55339366+hwan809@users.noreply.github.com>
2024-08-16 18:24:46 +0900

    add db + prepro

--- front/app.py
+++ front/app.py
@@ -8,11 +8,11 @@ from yeardistribution import YearDistribution
 
 from datetime import datetime, timedelta
 
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+# __import__('pysqlite3')
+# import sys
+# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
-import sqlite3
+# import sqlite3
 
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
